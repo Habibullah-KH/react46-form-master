c53e186481f56551b4f786a894c441df2bcfe75e
habibullah <habibullahkhandokar01@gmail.com>
2024-10-30 21:41:23 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Simpleform from './Components/simpleform/Simpleform'
+
+function App() {
+  return (
+    <>
+    <h1>Form practice</h1>
+      <Simpleform></Simpleform>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/simpleform/Simpleform.jsx
@@ -0,0 +1,13 @@
+const Simpleform = () => {
+    return (
+        <div>
+            <form>
+                <input type="text"/>
+                <br/>
+                <button>Submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default Simpleform;
